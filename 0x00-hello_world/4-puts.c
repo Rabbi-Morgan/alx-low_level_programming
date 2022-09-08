1f215934c00f546a4e53614fc7a5b6411f3f72de
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-09-08 17:09:21 +0100

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
-	return(0);
+	/* blah blah */
+	puts("\"Programming is like building a multilingual puzzle");
+	return( 0 );
 }
