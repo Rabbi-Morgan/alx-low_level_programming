881216ae1db0c5ff2c4d5f0536dd668d8f7e4db3
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-09-08 17:16:18 +0100

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-/* Prints using put */
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	/* blah blah */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
