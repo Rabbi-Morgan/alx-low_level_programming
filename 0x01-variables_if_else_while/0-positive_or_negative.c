de87f2693b54f340f4e0b10a49df1b974cae67ad
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-09-13 23:36:19 +0100

    correction to prev commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes 
- *main - something goes here
- * there */
+/**
+ * main - Entry point
+ * Description: 'print something to the terminal'
+ * Return: Always 0 (Success)
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -13,15 +15,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n < 0){
-	       printf("%d is negative\n", n);
-	}
-	else if (n == 0){
+	if (n < 0)
+	        printf("%d is negative\n", n);
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	}
-	else{
+	else
 		printf("%d is positive\n", n);
-	}
 	
-	return (0);
+	return(0);
 }
