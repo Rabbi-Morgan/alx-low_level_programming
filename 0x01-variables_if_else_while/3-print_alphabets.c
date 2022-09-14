4ac13bd52444d50aae561ea9658574a3c7fda9bc
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-09-14 12:15:52 +0100

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print out all alphabets
+ */
+
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
