2e069fc2ae33517ac5c9081c08ad6252d9228794
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-09-15 00:24:17 +0100

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Description: 'nothing to see here'
+ * Return: ''
+ */
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
+}
