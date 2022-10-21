d3070d2ba749892cc8649fa4a0f4034682e1312e
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-10-21 04:17:36 +0100

    new node

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - prepend new node
+ * @head: double pointer
+ * @str: pointer string
+ *
+ * Return: address of NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	newNode = malloc(sizeof(list_t));
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->len = len;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = newNode;
+
+	return (newNode);
+}
