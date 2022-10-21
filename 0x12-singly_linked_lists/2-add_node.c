2c426278b7353df2eb2f9448daa0c20a08fe1084
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-10-21 04:12:58 +0100

    hot fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newPtr;
-	unsigned int lenOfNode = 0;
+	unsigned int len = 0;
 
-	while (str[lenOfNode])
-		lenOfNode++;
+	while (str[len])
+		len++;
 
 	newPtr = malloc(sizeof(list_t));
 	if (!newPtr)
 		return (NULL);
 
 	newPtr->str = strdup(str);
-	newPtr->lenOfNode = lenOfNode;
+	newPtr->len = len;
 	newPtr->next = (*head);
 	(*head) = newPtr;
 
